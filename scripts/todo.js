3101140f128ed93a1ebddee157c1004d9cb94103
yukon-r <teacup6955@gmail.com>
2019-09-16 21:55:08 +0900

    list＆donelist空白時のメッセージ追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,11 +1,8 @@
 // Description:
 //   TODO を管理することができるボットです
+
 // Commands:
 //   ボット名 todo     - TODO を作成
-//   ボット名 done     - TODO を完了にする
-//   ボット名 del      - TODO を消す
-//   ボット名 list     - TODO の一覧表示
-//   ボット名 donelist - 完了した TODO の一覧表示
 'use strict';
 const todo = require('todo');
 module.exports = (robot) => {
@@ -14,20 +11,38 @@ module.exports = (robot) => {
 		todo.todo(task);
 		msg.send('追加しました: ' + task);
 	});
+
+//   ボット名 done     - TODO を完了にする
 	robot.respond(/done (.+)/i, (msg) => {
 		const task = msg.match[1].trim();
 		todo.done(task);
 		msg.send('完了にしました: ' + task);
 	});
+
+//   ボット名 del      - TODO を消す
 	robot.respond(/del (.+)/i, (msg) => {
 		const task = msg.match[1].trim();
 		todo.del(task);
 		msg.send('削除しました: ' + task);
 	});
+
+//   ボット名 list     - TODO の一覧表示
 	robot.respond(/list/i, (msg) => {
-		msg.send(todo.list().join('\n'));
-	});
-	robot.respond(/donelist/i, (msg) => {
-		msg.send(todo.donelist().join('\n'));
-	});
+    const list = todo.list();
+    if (list.length === 0) {
+      msg.send('(TODOはありません)');
+    } else {
+      msg.send(list.join('\n'));
+    }
+  });
+
+//   ボット名 donelist - 完了した TODO の一覧表示
+    robot.respond(/donelist/i, (msg) => {
+    const donelist = todo.donelist();
+    if (donelist.length === 0) {
+      msg.send('(完了したTODOはありません)');
+    } else {
+      msg.send(donelist.join('\n'));
+    }
+  });
 };
